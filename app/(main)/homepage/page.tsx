797ac773d307af67285258ec7c800a4bd9185e16
chuntaopeng <pengtaiwan6517@gmail.com>
2024-06-19 22:54:35 +0800

    feat:adding homepage info

--- app/(main)/homepage/page.tsx
+++ app/(main)/homepage/page.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Rankingchart from '@/components/Rankingchart'
 import News from '@/components/News'
+import Info from '@/components/Info'
 
 export default function page() {
   return (
-    <div className='m-1'>
+    <div className='m-1 flex flex-col items-center w-5/6 right-0 absolute'>
     <Rankingchart />
     <News />
+    <Info />
     
     </div>
   )
---
+++ components/Info.tsx
@@ -0,0 +1,85 @@
+import React from 'react'
+
+export default function Info() {
+  return (
+    <div className=" text-left my-12 w-5/6">
+        {/* build a white line */}
+      <div className="text-2xl font-bold mb-2">
+        血液排名
+      </div>
+      <div className="w-full h-0.5 bg-fuchsia-50 mx-auto mb-5"></div>
+      
+    
+      <div className=' w-full flex flex-row flex-wrap justify-between'>
+        <ul className=' w-full bg-white flex justify-center flex-wrap'>
+            <li className=' border-b-2 border-gray-300 min-h-[150px]  max-h-[300px] flex flex-row justify-around items-center p-7 flex-wrap'>
+                <div className=" flex-1 max-w-[40%] min-w-[225px] mr-2 overflow-hidden h-[200px] shadow-[0_3px_3px_rgba(0,0,0,0.4)]">
+                    <img src='https://www.metatech.com.tw/image/article-pic/chondrocyte-cell-sheet01.jpg' className='max-w-full h-96'/>
+
+                </div>
+                <div className="relative flex-1 flex flex-col items-start justify-center px-2 text-ellipsis">
+                    <span className='text-gray-50 inline-block text-[1.2rem] leading-none bg-[#044778] w-[100px] text-center my-[10px] py-[5px]'>
+                        膝軟骨
+                    </span>
+                    <span className='text-[22px] font-bold leading-[26px] tracking-[.015em] uppercase text-gray-600 inline-block mb-[10px] max-h-[50px] text-ellipsis overflow-hidden'>
+                      最新膝軟骨細胞層片治療法，真正解決您的膝關節困擾
+                    </span>
+                    <p className=' text-black'>
+                    隨著年齡增長，膝關節退化等問題浮現；又或是姿勢不良所導致的Ｏ型腿等，這些膝關節問題如症狀嚴重可
+                    能需到置換人工關節，但此種手術不僅傷口大，需要半年的恢復期，術後甚至無法激烈運動。而接下來就來介紹可解決這些問題的「膝軟骨細胞層片療法」，守護您的膝關節健康。
+                    </p>
+                    <a href="https://www.metatech.com.tw/tw/biomed/knowledge/chondrocyte-cell-sheet" className='text-black border border-black px-2 py-1'>
+                    繼續閱讀</a>
+                </div>
+
+            </li>
+            <li className=' border-b-2 border-gray-300 min-h-[150px]  max-h-[300px] flex flex-row justify-around items-center p-7 flex-wrap'>
+                <div className=" flex-1 max-w-[40%] min-w-[225px] mr-2 overflow-hidden h-[200px] shadow-[0_3px_3px_rgba(0,0,0,0.4)]">
+                    <img src='https://www.metatech.com.tw/image/article-pic/chondrocyte-cell-sheet01.jpg' className='max-w-full h-96'/>
+
+                </div>
+                <div className="relative flex-1 flex flex-col items-start justify-center px-2 text-ellipsis">
+                    <span className='text-gray-50 inline-block text-[1.2rem] leading-none bg-[#044778] w-[100px] text-center my-[10px] py-[5px]'>
+                        膝軟骨
+                    </span>
+                    <span className='text-[22px] font-bold leading-[26px] tracking-[.015em] uppercase text-gray-600 inline-block mb-[10px] max-h-[50px] text-ellipsis overflow-hidden'>
+                      最新膝軟骨細胞層片治療法，真正解決您的膝關節困擾
+                    </span>
+                    <p className=' text-black'>
+                    隨著年齡增長，膝關節退化等問題浮現；又或是姿勢不良所導致的Ｏ型腿等，這些膝關節問題如症狀嚴重可
+                    能需到置換人工關節，但此種手術不僅傷口大，需要半年的恢復期，術後甚至無法激烈運動。而接下來就來介紹可解決這些問題的「膝軟骨細胞層片療法」，守護您的膝關節健康。
+                    </p>
+                    <a href="https://www.metatech.com.tw/tw/biomed/knowledge/chondrocyte-cell-sheet" className='text-black border border-black px-2 py-1'>
+                    繼續閱讀</a>
+                </div>
+
+            </li>
+            <li className=' border-b-2 border-gray-300 min-h-[150px]  max-h-[300px] flex flex-row justify-around items-center p-7 flex-wrap'>
+                <div className=" flex-1 max-w-[40%] min-w-[225px] mr-2 overflow-hidden h-[200px] shadow-[0_3px_3px_rgba(0,0,0,0.4)]">
+                    <img src='https://www.metatech.com.tw/image/article-pic/chondrocyte-cell-sheet01.jpg' className='max-w-full h-96'/>
+
+                </div>
+                <div className="relative flex-1 flex flex-col items-start justify-center px-2 text-ellipsis">
+                    <span className='text-gray-50 inline-block text-[1.2rem] leading-none bg-[#044778] w-[100px] text-center my-[10px] py-[5px]'>
+                        膝軟骨
+                    </span>
+                    <span className='text-[22px] font-bold leading-[26px] tracking-[.015em] uppercase text-gray-600 inline-block mb-[10px] max-h-[50px] text-ellipsis overflow-hidden'>
+                      最新膝軟骨細胞層片治療法，真正解決您的膝關節困擾
+                    </span>
+                    <p className=' text-black'>
+                    隨著年齡增長，膝關節退化等問題浮現；又或是姿勢不良所導致的Ｏ型腿等，這些膝關節問題如症狀嚴重可
+                    能需到置換人工關節，但此種手術不僅傷口大，需要半年的恢復期，術後甚至無法激烈運動。而接下來就來介紹可解決這些問題的「膝軟骨細胞層片療法」，守護您的膝關節健康。
+                    </p>
+                    <a href="https://www.metatech.com.tw/tw/biomed/knowledge/chondrocyte-cell-sheet" className='text-black border border-black px-2 py-1'>
+                    繼續閱讀</a>
+                </div>
+
+            </li>
+
+        </ul>
+        
+
+      </div>
+    </div>
+  )
+}
